74f14f1ae8148c7ac294b5a29c1a501df1e4db4a
Steve Russo <64294847+sjrusso8@users.noreply.github.com>
2024-04-16 10:30:44 -0400

    chore: update readme & examples (#15)
    # Description
    
    - Update README with clearer "getting started" instructions
    - Update the SQL example to match this gist https://gist.github.com/sjrusso8/2b4e43af462367a15f91db5a33627449
    - Update other examples to explicitly set a remote URL 
    
    # Related Issue(s)
    <!---
    For example:
    
    - closes #106
    --->
    
    - closes #14

--- examples/delta.rs
+++ examples/delta.rs
@@ -12,7 +12,9 @@ use spark_connect_rs::dataframe::SaveMode;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let spark: SparkSession = SparkSessionBuilder::default().build().await?;
+    let spark: SparkSession = SparkSessionBuilder::remote("sc://127.0.0.1:15002/")
+        .build()
+        .await?;
 
     let paths = ["/opt/spark/examples/src/main/resources/people.csv"];
 
